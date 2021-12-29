eead561a5575a6a5c300d9f9d4909039b23401ad
ajhayden <55779059+ajhayden@users.noreply.github.com>
2021-12-29 00:08:20 -0700

    explain power of 2 question with more detail

--- Class 03 - Recursion/Recursion Homework/Homework - Power of 2  (level easy)/power_of_2_problem.py
+++ Class 03 - Recursion/Recursion Homework/Homework - Power of 2  (level easy)/power_of_2_problem.py
@@ -1,2 +1,15 @@
 # Given an integer n, return True if it is a power of 2. 
 # An integer n is a power of two, if there exists an integer x such that n == 2^x
+
+# Examples:
+
+# Input: n = 4
+# Output: True
+# 2^2 = 4
+
+# Input : n = 16
+# Output : True
+# 2^4 = 16
+
+# Input : n = 13
+# Output : False
