2f3a16efe4b37b704dc2547f730ac0f1794ad491
ajhayden <55779059+ajhayden@users.noreply.github.com>
2021-12-29 16:20:05 -0700

    add two solutions to array problems

---
+++ Class 04 - Array Problems Easy/Class Practice/best_time_to_buy_stock_solution.py
@@ -0,0 +1,35 @@
+# Say you have an array for which the ith element is the price of a given stock on day i
+
+# If you were only permitted to complete at most one transaction (i.e, buy one and 
+# sell one share of the stock) design an algorithm to find the maximum profit
+
+inputArray = [7, 1, 5, 3, 6, 4]
+
+def best_time_to_buy_stock(nums):
+    best_value = 0
+    
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            check_value = nums[j] - nums[i]
+            if check_value > best_value:
+                best_value = check_value
+    return best_value
+
+print(best_time_to_buy_stock(inputArray))
+
+# Optimized version that only uses one for loop
+def best_time_to_buy_stock_optimized(prices):
+    if len(prices) == 0:
+        return 0
+    
+    minSoFar = prices[0]
+    maxProfit = 0
+    for i in range(0, len(prices)):
+        if prices[i] < minSoFar:
+            minSoFar = prices[i]
+        if prices[i] - minSoFar > maxProfit:
+            maxProfit = prices[i] - minSoFar
+
+    return maxProfit
+
+print(best_time_to_buy_stock_optimized(inputArray))
---
+++ Class 04 - Array Problems Easy/Class Practice/two_sum_solution.py
@@ -0,0 +1,30 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target
+# You may assume that each input would have exactly one solution, and you may not use the same element twice
+
+def two_sum(arr, target):
+    for i in range(0, len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == target:
+                return [i, j]
+
+arr1 = [2, 5, 6, 3, 9] 
+target1 = 9
+print(two_sum(arr1, target1))
+
+# This is a more optimized version with O(n)
+def two_sum_with_dictionary(nums, target):
+    num_dict = {}
+
+    for i in range(0, len(nums)):
+        temp = target - nums[i]
+        if temp in num_dict.keys():
+            return [num_dict[temp], i]
+        num_dict[nums[i]] = i
+
+    return [0]
+
+nums = [2, 5, 6, 3, 9] 
+target2 = 9
+print(two_sum_with_dictionary(nums, target2))
+
+
