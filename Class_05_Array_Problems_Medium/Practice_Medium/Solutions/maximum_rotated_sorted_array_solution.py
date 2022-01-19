822a840fed7f20fa81140979e675e9c57e221970
ajhayden <55779059+ajhayden@users.noreply.github.com>
2022-01-18 18:39:01 -0700

    adding sorting days

---
+++ Class_05_Array_Problems_Medium/Practice_Medium/Solutions/maximum_rotated_sorted_array_solution.py
@@ -0,0 +1,37 @@
+# Suppose an array sorted in ascending order is rotated at some pivot unknown
+# to you beforehand. Find the maximum number in the sorted array. You may assume no duplicate exists in the array.
+# This problem came from leetcode.com
+
+input_array = [3, 4, 5, 6, 7, 1, 2]
+# Output = 6
+
+def maximum_rotated_sorted_array(nums):
+    maximum_result = nums[0]
+    i = 0
+    while i < len(nums):
+        if nums[i] > maximum_result:
+            maximum_result = nums[i]
+        i += 1
+    
+    return maximum_result
+
+print(maximum_rotated_sorted_array(input_array))
+
+# Optimized version
+def maximum_rotated_sorted_array_optimized(nums):
+    low = 0
+    high = len(nums) - 1
+
+    while low < high:
+        print("low", low)
+        print("high", high)
+        guess = (low + high) / 2
+        print("guess", guess)
+        if nums[guess] > nums[high]:
+            low = guess + 1
+        else:
+            high = guess
+
+    return nums[low]
+
+print(maximum_rotated_sorted_array_optimized(input_array))
