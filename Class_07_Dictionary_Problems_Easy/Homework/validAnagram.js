70e41e84f5416f1f02086ab764f106640c3625bf
Katie Bankhead <katie@bankheads.net>
2022-01-24 20:23:19 -0700

    Update homework prompt

--- Class_07_Dictionary_Problems_Easy/Homework/validAnagram.js
+++ Class_07_Dictionary_Problems_Easy/Homework/validAnagram.js
@@ -1,5 +1,5 @@
 /**
- * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise. Use a dictionary.
  * 
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
  * typically using all the original letters exactly once.
@@ -9,6 +9,8 @@
  * 
  * Input: s = "rat", t = "car"
  * Output: false
+ * 
+ * Source: https://leetcode.com/problems/valid-anagram/
  */
 
 const s = 'anagram'
