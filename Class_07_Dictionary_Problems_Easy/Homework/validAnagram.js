590db07aee399999cee2fb201f3ff764bd53a728
Katie Bankhead <katie@bankheads.net>
2022-01-24 20:13:26 -0700

    Add dictionary easy homework

---
+++ Class_07_Dictionary_Problems_Easy/Homework/validAnagram.js
@@ -0,0 +1,15 @@
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * 
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+ * typically using all the original letters exactly once.
+ * 
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * 
+ * Input: s = "rat", t = "car"
+ * Output: false
+ */
+
+const s = 'anagram'
+const t = 'nagaram'
