e35a83af9fc5b79ac8aaf7f44bc5536366a17f35
kaylapexton <56372142+kaylapexton@users.noreply.github.com>
2022-03-29 18:33:59 -0600

    Update sum_with_recursion_solution.py

--- Unit_08_Midterm/Solutions/sum_with_recursion_solution.py
+++ Unit_08_Midterm/Solutions/sum_with_recursion_solution.py
@@ -1,6 +1,31 @@
 # Write a function that calculates the sum of an array of numbers using recursion. 
 # You must use recursion to gain full credit for this question.
 
-# Example:
-# input = [3,6,2,9,9,4]
-# output = 33
+input = [3,6,2,9,9,4]
+total = 0
+def sum(listofNum):
+
+  #check to see if list length is geater than 
+  if len(listofNum) > 1:
+
+    # add the first and last elements together
+    total =  listofNum[len(listofNum) - 1] +  listofNum[0]
+
+    # remove the added elements
+    listofNum.pop(len(listofNum) -1)
+
+    listofNum.pop(0)
+
+    # return added elements and call the function to act on the rest of the list
+    return total + sum(listofNum)
+
+    # if list has odd number of elements add the last one
+  elif len(listofNum) == 1:
+
+    return listofNum[0]
+
+    # end conidtion add 0
+  else:
+    return 0
+
+print(sum(input))
